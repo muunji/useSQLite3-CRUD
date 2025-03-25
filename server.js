6062f9113c63b5fbfbc92246edc3851d7dacab01
김민지 <qniomo00@gmail.com>
2025-03-25 15:23:11 +0900

    DB서버에서 DB  연결 시도

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 //데이터베이스 연결
+const db = new sqlite3.Database('data.db', (err) => {
+  if (err) {
+    console.log('DB서버에서 DB 연결 실패',err)
+  } else {
+    console.log('DB서버에서 DB 연결 성공')
+  }
+})
 
 //post 데이터 추가
 
