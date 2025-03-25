3dd272c5407cf983b71f921573d5f12d6e3017f0
김민지 <qniomo00@gmail.com>
2025-03-25 15:21:05 +0900

    express 가져옴

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //데이터베이스 서버
+import express from 'express'
+import sqlite3 from 'sqlite3'
 
 //express 가져옴
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //데이터베이스 연결
 
