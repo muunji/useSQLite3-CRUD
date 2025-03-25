b4e129280cae54137c52db288ff05482904739b1
김민지 <qniomo00@gmail.com>
2025-03-25 15:11:48 +0900

    데이터베이스 연결 시도

--- database.js
+++ database.js
@@ -1,6 +1,18 @@
 import sqlite3 from 'sqlite3'
 
 //데이터베이스 생성 함수
-
-//데이터베이스 연결
-//테이블 생성
+export function makeDB() {
+  //데이터베이스 연결
+  const db = new sqlite3.Database('data.db', (err) => {
+    if (err) {
+      console.log('DB 연결 실패',err)
+    } else {
+      db.run(`
+        
+        `)
+    }
+  })
+  
+  //테이블 생성
+  
+}
