650b940e791d74d67e80f640ddf690573929e5b9
muunji <twoji3019@naver.com>
2025-03-30 13:32:12 +0900

    input 태그 식별 오류 수정

--- public/makeList.js
+++ public/makeList.js
@@ -108,10 +108,11 @@ function putOriginToForm(id) {
         item: data.item
       }
       //input창 가져오기
-      const editInput = document.querySelectorAll('div#edit > input')
+      const editInput = document.querySelectorAll('div#edit input')
       //input value에 기본 내용 넣어주기
-      editInput[0].value = `${origin.date}T${origin.time}`
-      editInput[1].value = origin.item
+      // editInput[0].value = `${origin.date}T${origin.time}`
+      // editInput[1].value = origin.item
+      console.log(editInput[0])
     })
 
 }
